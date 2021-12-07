ffa55ed53136623f7d0d80f2526fd5a94934122c
Onwuzor Victor <onwuzorvictor@outlook.com>
2021-12-07 01:52:06 +0100

    add db to context

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const { ApolloServer } = require('apollo-server')
 const typeDefs = require('./typedefs')
 const resolvers = require('./resolvers')
+const db = require('./db')
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context({ req, connection }) {
+    const context = { ...db }
+    return { ...context }
+  },
 })
 
 server.listen(4000).then(({url}) => {
