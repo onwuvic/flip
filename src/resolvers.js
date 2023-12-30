cb18fbe35d0db7f72e19108aa0a5fc7b5153f1f3
Onwuzor Victor <onwuzorvictor@outlook.com>
2023-12-30 09:54:53 +0100

    use test data for resolvers

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,54 +1,58 @@
+const { AuthenticationError } = require("apollo-server");
+
 module.exports = {
   Query: {
-    me() {
-      return {
-        id: '1',
-        email: 'john.doe@we.com',
-        avatar: 'https://www.gravatar.com/avatar/d4c74594d841139328695756648b6bd6',
-        verified: true,
-        createdAt: '2018-04-04T18:25:43.511Z'
-      };
+    me(_, __, { user }) {
+      return user;
     },
     posts: () => {
-      return [{
-        id: '7388382dhhd',
-        message: 'This should be it all day',
-        createdAt: '2020-01-01T00:00:00.000Z',
-        likes: 6,
-        views: 10,
-      }]
-    }
-  },
-  User: {
-    posts: (parent, args, ctx, info) => {
       return [
         {
-          id: '7388382dhhd',
-          message: 'This should be it all day 2 6',
-          author: 'John Doe',
-          createdAt: '2020-01-01T00:00:00.000Z',
+          id: "7388382dhhd",
+          message: "This should be it all day",
+          createdAt: "2020-01-01T00:00:00.000Z",
           likes: 6,
           views: 10,
-        }
-      ]
+        },
+      ];
     },
-    role: () => 'ADMIN',
-    settings: () => ({
-      id: '1',
-      user: 'John Doe',
-      emailNotifications: true,
-      pushNotifications: true,
-    })
+  },
+  Mutation: {
+    signup: (_, { input }, { models, createToken }) => {
+      const existing = models.User.findOne({ email: input.email });
+
+      if (existing) {
+        throw new AuthenticationError("User already exists");
+      }
+
+      const user = models.User.create({
+        ...input,
+        verified: true,
+        avatar: "https://www.gravatar.com/avatar/d4c74594d841139328695756648b6bd6",
+      });
+
+      const token = createToken(user);
+      return { user, token };
+    },
+  },
+  User: {
+    posts: (parent, args, { models }, info) => {
+      return models.Post.findMany({ author: "XUYFfLuWeJWrwPr4TRajF" });
+    },
+    settings: (parent, args, { models }, info) => {
+      return models.Setting.findOne({ user: "XUYFfLuWeJWrwPr4TRajF" });
+    },
   },
   Post: {
-    author: (parent, args, ctx, info) => {
+    author: (parent, args, { models }, info) => {
       return {
-        id: '1',
-        email: 'john.doe@we.com',
-        avatar: 'https://www.gravatar.com/avatar/d4c74594d841139328695756648b6bd6',
+        id: "1",
+        email: "john.doe@we.com",
+        avatar:
+          "https://www.gravatar.com/avatar/d4c74594d841139328695756648b6bd6",
         verified: true,
-        createdAt: '2018-04-04T18:25:43.511Z',
-      }
-    }
-  }
-}
+        createdAt: "2018-04-04T18:25:43.511Z",
+      };
+    },
+  },
+};
